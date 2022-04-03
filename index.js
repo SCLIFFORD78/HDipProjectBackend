2ba15c41e74d30aff9aa31ed1f2147c0d58ba329
SCLIFFORD78 <faugh01@gmail.com>
2022-04-03 23:36:21 +0100

    still trouble shooting deployment

--- index.js
+++ index.js
@@ -25,7 +25,16 @@ env.config();
  */
 const server2 = Hapi.server({
   port: process.env.PORT || 4001,
-  routes: { cors: true },
+  routes: {
+    cors: {
+        origin: ['*'], // an array of origins or 'ignore'
+        headers: ['Authorization'], // an array of strings - 'Access-Control-Allow-Headers'
+        exposedHeaders: ['Accept'], // an array of exposed headers - 'Access-Control-Expose-Headers',
+        additionalExposedHeaders: ['Accept'], // an array of additional exposed headers
+        maxAge: 60,
+        credentials: true // boolean - 'Access-Control-Allow-Credentials'
+    }
+},
 });
 
 async function init() {
