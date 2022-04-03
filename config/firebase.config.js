f8f3bfccc45f9d4cc46a5afbcd2cc12df95d9a76
SCLIFFORD78 <faugh01@gmail.com>
2022-04-03 23:30:16 +0100

    testing deployment issues

---
+++ config/firebase.config.js
@@ -0,0 +1,10 @@
+module.exports ={
+    apiKey: "AIzaSyCY8VnDgpIM0CGKRy6ZSnPnbR_V3X77jMQ",
+    authDomain: "hdip-65317.firebaseapp.com",
+    databaseURL: "https://hdip-65317-default-rtdb.firebaseio.com",
+    projectId: "hdip-65317",
+    storageBucket: "hdip-65317.appspot.com",
+    messagingSenderId: "662261644857",
+    appId: "1:662261644857:web:c0baafb4920393093497e8",
+    measurementId: "G-L966EHJ23D"
+}
