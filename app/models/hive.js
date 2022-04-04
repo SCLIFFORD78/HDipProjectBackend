8740c322d0889dd07a21873369e3b3fdd9b21f90
SCLIFFORD78 <faugh01@gmail.com>
2022-04-04 16:10:21 +0100

    updated hive model for alarms

--- app/models/hive.js
+++ app/models/hive.js
@@ -13,6 +13,8 @@ const Hive = {
   dateRegistered:  Math.floor(Date.now() / 1000).toString(),
   sensorNumber: "",
   location: {lat:0.0, lng:0.0, zoom:15},
+  tempAlarm: 18,
+  alarmEvents: "",
   details: []
 }
 module.exports = Hive;
