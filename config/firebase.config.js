8c102232a39949d31067e111688edbb684663f25
SCLIFFORD78 <faugh01@gmail.com>
2022-04-12 09:52:36 +0100

    adding firebase config

---
+++ config/firebase.config.js
@@ -0,0 +1,15 @@
+const env = require("dotenv");
+env.config();
+
+
+
+module.exports ={
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.authDomain,
+    databaseURL: process.env.databaseURL,
+    projectId: process.env.projectId,
+    storageBucket: process.env.storageBucket,
+    messagingSenderId: process.env.messagingSenderId,
+    appId: process.env.appId,
+    measurementId: process.env.measurementId
+}
--- config/test.js
+++
@@ -1,2 +0,0 @@
-const json = require("./hdip-65317-firebase-adminsdk-3auua-1d23de656e.json")
-console.log(JSON.stringify(json));
