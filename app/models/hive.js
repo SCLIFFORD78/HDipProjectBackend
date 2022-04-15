ae757c9fafacebeac2ea5cdefac92015c731f8a1
SCLIFFORD78 <faugh01@gmail.com>
2022-04-15 23:49:55 +0100

    file tidy up

--- app/models/hive.js
+++ app/models/hive.js
@@ -14,10 +14,6 @@ const Hive = {
   sensorNumber: "",
   location: {lat:0.0, lng:0.0, zoom:15},
   tempAlarm: 18,
-<<<<<<< HEAD
-  alarmEvents: "",
-=======
->>>>>>> master
 }
 module.exports = Hive;
 
